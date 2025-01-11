cf06f7da9771a2a090adb8389b44ff6a4ec1f92f
Almas3211 <bhattialmas77@gmail.com>
2025-01-11 07:35:08 +0000

    eserciziomax

--- esercizi.cpp
+++ esercizi.cpp
@@ -5,24 +5,28 @@ int main() {
 
 int m[3][3];
 
-int i,j,p=1;
+int i,j,p=1,max=0;
 
 for (i=0;i<3;i++) {
     for (j=0;i<3;j++) {
         cout << "inserisci un numero nell posizione " <<i<< " e " <<j<<endl;
         cin >> m[i][j];
     }
-      cout<<"\n";
+      
 }
 for (i=0;i<3;i++) { 
     for (j=0;i<3;j++) {
         if (m[i]==m[j]) { 
             p=p*m[i][j];
+            if(m[i][j]>max) { 
+                max=m[i][j];
+            }
             }
 
     }
     
 }
-cout<<"Il prodotto di tutti i numeri sulla diagonale è di ";cout<<p;
+cout<<"Il prodotto dei numeri sulla diagonale è di ";cout<<p;
+cout<<"Il max è ";cout<<max;
 
 }
