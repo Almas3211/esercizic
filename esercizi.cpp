ada755046887e3a9501d76c0562d39a64aa3c9a0
Almas3211 <bhattialmas77@gmail.com>
2025-01-11 07:31:07 +0000

    esercizio

--- esercizi.cpp
+++ esercizi.cpp
@@ -3,18 +3,19 @@ using namespace std;
 
 int main() {
 
-int m[2][3];
+int m[3][3];
 
 int i,j,p=1;
 
 for (i=0;i<3;i++) {
     for (j=0;i<3;j++) {
-        cout << "inserisci un numero nell posizione " <<i<< " e " <<j<<;
+        cout << "inserisci un numero nell posizione " <<i<< " e " <<j<<endl;
         cin >> m[i][j];
     }
+      cout<<"\n";
 }
 for (i=0;i<3;i++) { 
-    for (j=0;i<3,j++) {
+    for (j=0;i<3;j++) {
         if (m[i]==m[j]) { 
             p=p*m[i][j];
             }
@@ -22,6 +23,6 @@ for (i=0;i<3;i++) {
     }
     
 }
-cout << "il prodotto dei numeri è "; cout <<p;
+cout<<"Il prodotto di tutti i numeri sulla diagonale è di ";cout<<p;
 
 }
